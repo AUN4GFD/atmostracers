0a583ccd0a8ee32f67831953089d9523cffdc4a5
Max H. Balsmeier <mhbalsmeier@gnail.com>
2020-11-09 12:43:49 +0100

    Bugfixes in phase transition rates.

--- src/atmostracers.c
+++ src/atmostracers.c
@@ -12,7 +12,7 @@ Github repository: https://github.com/MHBalsmeier/atmostracers
 #define R (N_A*K_B)
 #define R_V (R/M_V)
 #define T_0 273.15
-#define EPSILON 1e-10
+#define EPSILON (1e-10)
 #define DENSITY_WATER 1024.0
 #define GRAVITY 9.8
 
@@ -46,79 +46,179 @@ double ret_phase_trans_heat(int direction, double temperature)
     */
     double result;
     if (direction == 0)
+    {
         result = 2257000;
+	}
     if (direction == 1)
+    {
         result = 2257000 + 333500;
+	}
     if (direction == 2)
+    {
         result = 333500;
+	}
     return result;
 }
 
-int calc_h2otracers_source_rates(double mass_source_rates[], double heat_source_rates[], double densities[], double tracer_temperature_densities[], double temperature[], int number_of_tracers, int number_of_scalars, double delta_t)
+int calc_h2otracers_source_rates(double mass_source_rates[], double heat_source_rates[], double densities[], double tracer_temperature_densities[], double temperature[], int number_of_scalars, double delta_t)
 {
+	/*
+	It assumes the following order for the constituents:
+	ice - liquid water - dry air - water vapour
+	*/
     double diff_density, phase_trans_density, saturation_pressure, water_vapour_pressure, solid_temperature, liquid_temperature;
+    // loop over all grid boxes
     for (int i = 0; i < number_of_scalars; ++i)
     {
+    	// determining the temperature of the ice
     	if (densities[i] < EPSILON)
-    		solid_temperature = 0;
+    	{
+    		solid_temperature = T_0;
+		}
     	else
+    	{
     		solid_temperature = tracer_temperature_densities[i]/densities[i];
+		}
+		
+		// determining the temperature of the liquid water
     	if (densities[number_of_scalars + i] < EPSILON)
-    		liquid_temperature = 0;
+    	{
+    		liquid_temperature = T_0;
+		}
     	else
+    	{
     		liquid_temperature = tracer_temperature_densities[number_of_scalars + i]/densities[number_of_scalars + i];
-        if (temperature[i] > T_0)
+		}
+		
+		// determining the saturation pressure
+        if (temperature[i] >= T_0)
+    	{
             saturation_pressure = saturation_pressure_over_water(temperature[i]);
+		}
         else
+    	{
             saturation_pressure = saturation_pressure_over_ice(temperature[i]);
-        water_vapour_pressure = densities[2*number_of_scalars + i]*R_V*temperature[i];
-        if (water_vapour_pressure <= saturation_pressure)
+		}
+		
+		// determining the water vapour pressure
+        water_vapour_pressure = densities[3*number_of_scalars + i]*R_V*temperature[i];
+        
+    	// the amount of water vapour that the air can still take 
+        diff_density = (saturation_pressure - water_vapour_pressure)/(R_V*temperature[i]);
+        
+        // the case where the air is not over-saturated
+        if (diff_density >= 0)
         {
-            diff_density = (saturation_pressure - water_vapour_pressure)/(R_V*temperature[i]);
+            // temperature >= 0 °C
             if (temperature[i] >= T_0)
             {
+            	// It is assumed that the still present ice vanishes within one time step.
                 mass_source_rates[i] = -densities[i]/delta_t;
+                
+                // The amount of liquid water per volume that will evaporate.
+                // In case the air cannot take all the water, not everything will evaporate.
                 phase_trans_density = fmin(densities[number_of_scalars + i], diff_density);
+                
+                // The source rate for the liquid water consists of two terms:
+                // 1.) the evaporation
+                // 2.) the melting of ice
                 mass_source_rates[number_of_scalars + i] = (densities[i] - phase_trans_density)/delta_t;
-                mass_source_rates[2*number_of_scalars + i] = phase_trans_density/delta_t;
-                heat_source_rates[i] = mass_source_rates[i]*ret_phase_trans_heat(1, solid_temperature);
-                heat_source_rates[number_of_scalars + i] = mass_source_rates[number_of_scalars + i]*ret_phase_trans_heat(1, T_0);
-                heat_source_rates[2*number_of_scalars + i] = 0;
+                
+                // the tendency for the water vapour
+                mass_source_rates[3*number_of_scalars + i] = phase_trans_density/delta_t;
+                
+                // the heat source rates acting on the ice
+                heat_source_rates[i] = mass_source_rates[i]*ret_phase_trans_heat(2, solid_temperature);
+                
+                // the heat source rates acting on the liquid water
+                heat_source_rates[number_of_scalars + i] =
+                // the evaporation
+                -phase_trans_density*ret_phase_trans_heat(0, T_0)/delta_t;
             }
+            // temperature < 0 °C
             else
             {
+            	// Everything that can sublimate will sublimate.
                 phase_trans_density = fmin(densities[i], diff_density);
+                
+                // the tendency for the ice contains two terms
+                // 1.) the freezing
+                // 2.) the phase transition through sublimation
                 mass_source_rates[i] = (densities[number_of_scalars + i] - phase_trans_density)/delta_t;
+                
+            	// It is assumed that the still present liquid water vanishes within one time step.
                 mass_source_rates[number_of_scalars + i] = -densities[number_of_scalars + i]/delta_t;
-                mass_source_rates[2*number_of_scalars + i] = phase_trans_density/delta_t;
-                heat_source_rates[i] = (densities[number_of_scalars + i]*ret_phase_trans_heat(2, solid_temperature) - phase_trans_density*ret_phase_trans_heat(1, solid_temperature))/delta_t;
-                heat_source_rates[number_of_scalars + i] = mass_source_rates[number_of_scalars + i]*ret_phase_trans_heat(1, liquid_temperature);
-                heat_source_rates[2*number_of_scalars + i] = 0;
+                
+                // the tendency for the water vapour
+                mass_source_rates[3*number_of_scalars + i] = phase_trans_density/delta_t;
+                
+                // the heat source rates acting on the ice
+                heat_source_rates[i] = (
+                // the freezing
+                densities[number_of_scalars + i]*ret_phase_trans_heat(2, solid_temperature)
+                // the sublimation
+                - phase_trans_density*ret_phase_trans_heat(1, solid_temperature))/delta_t;
+                
+                // the heat source rates acting on the liquid water
+                heat_source_rates[number_of_scalars + i] = 0;
             }
         }
+        // the case where the air is over-saturated
         else
         {
-            diff_density = (water_vapour_pressure - saturation_pressure)/(R_V*temperature[i]);
-            mass_source_rates[2*number_of_scalars + i] = -diff_density/delta_t;
+        	// the vanishing of water vapour through the phase transition
+            mass_source_rates[3*number_of_scalars + i] = diff_density/delta_t;
+            // temperature >= 0 °C
             if (temperature[i] >= T_0)
             {
+            	// It is assumed that the still present ice vanishes within one time step.
                 mass_source_rates[i] = -densities[i]/delta_t;
-                mass_source_rates[number_of_scalars + i] = (diff_density + densities[i])/delta_t;
-                mass_source_rates[2*number_of_scalars + i] = -diff_density/delta_t;
+                
+                // The source rate for the liquid water consists of two terms:
+                // 1.) the condensation
+                // 2.) the melting of ice
+                mass_source_rates[number_of_scalars + i] = (-diff_density + densities[i])/delta_t;
+                
+                // the tendency for water vapour (through condensation)
+                mass_source_rates[3*number_of_scalars + i] = diff_density/delta_t;
+                
+                // the heat source rates acting on the ice
                 heat_source_rates[i] = -densities[i]*ret_phase_trans_heat(2, solid_temperature)/delta_t;
-                heat_source_rates[number_of_scalars + i] = mass_source_rates[number_of_scalars + i]*ret_phase_trans_heat(1, liquid_temperature);
-                heat_source_rates[2*number_of_scalars + i] = 0;
+                
+                // the heat source rates acting on the liquid water
+                heat_source_rates[number_of_scalars + i] =
+                // it is only affected by the condensation
+                -diff_density*ret_phase_trans_heat(1, liquid_temperature)/delta_t;
             }
+            // temperature < 0 °C
             else
             {
-                mass_source_rates[i] = (diff_density + densities[number_of_scalars + i])/delta_t;
+                // The source rate for the ice consists of two terms:
+                // 1.) the resublimation
+                // 2.) the melting of ice
+                mass_source_rates[i] = (-diff_density + densities[number_of_scalars + i])/delta_t;
+                
+                // It is assumed that the liquid water disappears within one time step.
                 mass_source_rates[number_of_scalars + i] = -densities[number_of_scalars + i]/delta_t;
-                mass_source_rates[2*number_of_scalars + i] = -diff_density/delta_t;
-                heat_source_rates[i] = (diff_density*ret_phase_trans_heat(1, solid_temperature) + densities[number_of_scalars + i]*ret_phase_trans_heat(2, solid_temperature))/delta_t;
-                heat_source_rates[number_of_scalars + i] = mass_source_rates[number_of_scalars + i]*ret_phase_trans_heat(1, liquid_temperature);
-                heat_source_rates[2*number_of_scalars + i] = 0;
+                
+                // the tendency for water vapour (resublimation)
+                mass_source_rates[3*number_of_scalars + i] = diff_density/delta_t;
+                
+                // the heat source rates acting on the ice
+                heat_source_rates[i] =
+                // the component through the resublimation
+                (-diff_density*ret_phase_trans_heat(1, solid_temperature)
+                // the component through freezing
+                + densities[number_of_scalars + i]*ret_phase_trans_heat(2, solid_temperature))/delta_t;
+                
+                // the heat source rates acting on the liquid water
+                heat_source_rates[number_of_scalars + i] = 0;
             }
         }
+        
+        // The latent heat does not affect the gas phase.
+        heat_source_rates[2*number_of_scalars + i] = 0;
+        heat_source_rates[3*number_of_scalars + i] = 0;
     }
     return 0;
 }
@@ -164,9 +264,13 @@ double ret_c_p_cond(int solid_or_liquid, int subcategory, double temp)
 {
     double result;
     if (solid_or_liquid == 0)
+    {
         result = 2060;
+    }
     if (solid_or_liquid == 1)
+    {
         result = 4184;
+    }
     return result;
 }
 
@@ -174,9 +278,13 @@ double ret_c_v_cond(int solid_or_liquid, int subcategory, double temp)
 {
     double result;
     if (solid_or_liquid == 0)
+    {
         result = 2060;
+    }
     if (solid_or_liquid == 1)
+    {
         result = 4184;
+    }
     return result;
 }
 
